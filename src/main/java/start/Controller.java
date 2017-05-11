ce2b7051064873ee1dc303ee86fa922f66a62376
Vitalii <flipsun@gmail.com>
2017-05-11 15:50:56 +0300

    filechooser

--- src/main/java/start/Controller.java
+++ src/main/java/start/Controller.java
@@ -1,8 +1,23 @@
 package start;
 
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
+import javax.swing.text.html.ListView;
+import java.io.File;
+
 /**
  * Created by Vitalii on 10.05.2017.
  */
 public class Controller{
 
+    private ListView listView;
+
+    public void OpenDialog(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        File selectFile = fileChooser.showOpenDialog(null);
+        if (selectFile !=null){
+            System.out.println("1");
+        }
+    }
 }
